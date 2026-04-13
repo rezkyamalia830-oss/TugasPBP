be196a92ebee90a53c2d5735dc2d7e1968a8b5a1
nekyoink <nekyoink@gmail.com>
2026-04-13 12:20:05 +0800

    hapus file

--- fungtion.php
+++
@@ -1,9 +0,0 @@
-<?php
-function tambah($var1, $var2) {
-    return $var1+$var2;
-}
-$hasil=tambah(10, 20);
-        echo "Hasil tambah: $hasil"; 
-    $tanggal_sekarang=date("Y-m-d  H:i:s");
-        echo "\nTanggal sekarang: $tanggal_sekarang";
-?>
